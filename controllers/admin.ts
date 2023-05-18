7cec34d116f9ad43daa4d4a84743bf6524a4fdaa
amir25n <amir25nk@gmail.com>
2023-05-18 16:51:46 +0330

    refactor (use async intead of promise)

--- controllers/admin.ts
+++ controllers/admin.ts
@@ -2,25 +2,32 @@ import { Request, Response } from "express";
 
 import Todo from "../model/add-todo.js";
 
-export const addTodo = (req: Request, res: Response) => {
+export const addTodo = async (req: Request, res: Response) => {
     if (!req.body.todo) return res.redirect("/");
 
-    Todo.create({ text: req.body.todo })
-        .then(() => res.redirect("/"))
-        .catch((err) => console.log(err));
+    try {
+        await Todo.create({ text: req.body.todo });
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+    }
 };
-export const deleteTodo = (req: Request, res: Response) => {
-    Todo.destroy({ where: { id: req.params.id } })
-        .then(() => res.redirect("/"))
-        .catch((err) => console.log(err));
+export const deleteTodo = async (req: Request, res: Response) => {
+    try {
+        await Todo.destroy({ where: { id: req.params.id } });
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+    }
 };
 
-export const completeTodo = (req: Request, res: Response) => {
-    Todo.findByPk(req.params.id)
-        .then((todo: any) => {
-            todo.completed = true;
-            return todo.save();
-        })
-        .then(() => res.redirect("/"))
-        .catch((err) => console.log(err));
+export const completeTodo = async (req: Request, res: Response) => {
+    try {
+        const todo: any = await Todo.findByPk(req.params.id);
+        todo.completed = true;
+        todo.save();
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+    }
 };
--- controllers/fetchAll.ts
+++ controllers/fetchAll.ts
@@ -2,16 +2,16 @@ import { Request, Response } from "express";
 
 import Todo from "../model/add-todo.js";
 
-export const indexRoutes = (req: Request, res: Response) => {
-    Todo.count({ where: { completed: true } })
-        .then((completedTodo) => {
-            Todo.findAll().then((todos) => {
-                res.render("index", {
-                    todos,
-                    completedTodo,
-                    reminingTodo: todos.length - completedTodo,
-                });
-            });
-        })
-        .catch((err) => console.log(err));
+export const indexRoutes = async (req: Request, res: Response) => {
+    try {
+        const todos = await Todo.findAll();
+        const completedTodo = await Todo.count({ where: { completed: true } });
+        res.render("index", {
+            todos,
+            completedTodo,
+            reminingTodo: todos.length - completedTodo,
+        });
+    } catch (err) {
+        console.log(err);
+    }
 };
